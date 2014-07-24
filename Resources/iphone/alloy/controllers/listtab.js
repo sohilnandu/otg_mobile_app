b0a43bb11540818f35a8b27dd89b8cef3f92e0fa
Mami Nomura <maminomura@maminobako.local>
2014-07-24 15:32:06 -0700

    Added search and styled list

--- Resources/iphone/alloy/controllers/listtab.js
+++ Resources/iphone/alloy/controllers/listtab.js
@@ -1,37 +1,31 @@
-function __processArg(obj, key) {
-    var arg = null;
-    if (obj) {
-        arg = obj[key] || null;
-        delete obj[key];
-    }
-    return arg;
-}
-
 function Controller() {
-    function __alloyId23(e) {
+    function __alloyId26(e) {
         if (e && e.fromAdapter) return;
-        var opts = __alloyId23.opts || {};
-        var models = __alloyId22.models;
+        var opts = __alloyId26.opts || {};
+        var models = __alloyId25.models;
         var len = models.length;
-        var __alloyId18 = [];
+        var __alloyId21 = [];
         for (var i = 0; len > i; i++) {
-            var __alloyId19 = models[i];
-            __alloyId19.__transform = doTransform(__alloyId19);
-            var __alloyId21 = {
-                template: "undefined" != typeof __alloyId19.__transform["template"] ? __alloyId19.__transform["template"] : __alloyId19.get("template"),
+            var __alloyId22 = models[i];
+            __alloyId22.__transform = doTransform(__alloyId22);
+            var __alloyId24 = {
+                template: "undefined" != typeof __alloyId22.__transform["template"] ? __alloyId22.__transform["template"] : __alloyId22.get("template"),
                 name: {
-                    text: "undefined" != typeof __alloyId19.__transform["name"] ? __alloyId19.__transform["name"] : __alloyId19.get("name")
+                    text: "undefined" != typeof __alloyId22.__transform["name"] ? __alloyId22.__transform["name"] : __alloyId22.get("name")
                 },
                 dollarSign: {
-                    text: "undefined" != typeof __alloyId19.__transform["dollarSign"] ? __alloyId19.__transform["dollarSign"] : __alloyId19.get("dollarSign")
+                    text: "undefined" != typeof __alloyId22.__transform["dollarSign"] ? __alloyId22.__transform["dollarSign"] : __alloyId22.get("dollarSign")
                 },
                 cityState: {
-                    text: "undefined" != typeof __alloyId19.__transform["cityState"] ? __alloyId19.__transform["cityState"] : __alloyId19.get("cityState")
+                    text: "undefined" != typeof __alloyId22.__transform["cityState"] ? __alloyId22.__transform["cityState"] : __alloyId22.get("cityState")
+                },
+                properties: {
+                    searchableText: "undefined" != typeof __alloyId22.__transform["name"] ? __alloyId22.__transform["name"] : __alloyId22.get("name")
                 }
             };
-            __alloyId18.push(__alloyId21);
+            __alloyId21.push(__alloyId24);
         }
-        opts.animation ? $.__views.section.setItems(__alloyId18, opts.animation) : $.__views.section.setItems(__alloyId18);
+        opts.animation ? $.__views.section.setItems(__alloyId21, opts.animation) : $.__views.section.setItems(__alloyId21);
     }
     function doTransform(model) {
         var o = model.toJSON();
@@ -45,49 +39,39 @@ function Controller() {
     }
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
     this.__controllerPath = "listtab";
-    if (arguments[0]) {
-        __processArg(arguments[0], "__parentSymbol");
-        __processArg(arguments[0], "$model");
-        __processArg(arguments[0], "__itemTemplate");
-    }
+    arguments[0] ? arguments[0]["__parentSymbol"] : null;
+    arguments[0] ? arguments[0]["$model"] : null;
+    arguments[0] ? arguments[0]["__itemTemplate"] : null;
     var $ = this;
     var exports = {};
     Alloy.Collections.instance("donor");
     $.__views.__alloyId5 = Ti.UI.createWindow({
         backgroundColor: "white",
-        title: "ListView",
+        title: "United Zombie College Funds",
         id: "__alloyId5"
     });
-    var __alloyId6 = {};
-    var __alloyId8 = [];
-    var __alloyId9 = {
-        type: "Ti.UI.Label",
-        bindId: "dollarSign",
-        properties: {
-            left: 60,
-            color: "black",
-            right: 300,
-            top: 6,
-            width: 48,
-            height: 48,
-            bindId: "dollarSign"
-        }
-    };
-    __alloyId8.push(__alloyId9);
+    $.__views.__alloyId6 = Ti.UI.createSearchBar({
+        barColor: "#000",
+        id: "__alloyId6"
+    });
+    var __alloyId7 = {};
+    var __alloyId9 = [];
     var __alloyId10 = {
         type: "Ti.UI.Label",
         bindId: "name",
         properties: {
             left: 60,
             color: "black",
-            top: 5,
+            top: 10,
             font: {
-                fontWeight: "bold"
+                fontWeight: "bold",
+                fontFamily: "Helvetica Neue",
+                fontSize: 15
             },
             bindId: "name"
         }
     };
-    __alloyId8.push(__alloyId10);
+    __alloyId9.push(__alloyId10);
     var __alloyId11 = {
         type: "Ti.UI.Label",
         bindId: "cityState",
@@ -95,90 +79,115 @@ function Controller() {
             left: 60,
             color: "black",
             top: 25,
+            bottom: 5,
+            font: {
+                fontFamily: "Helvetica Neue",
+                fontSize: 12
+            },
             bindId: "cityState"
         }
     };
-    __alloyId8.push(__alloyId11);
-    var __alloyId7 = {
-        properties: {
-            height: "60",
-            name: "checkedInTemplate",
-            accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_CHECKMARK
-        },
-        childTemplates: __alloyId8
-    };
-    __alloyId6["checkedInTemplate"] = __alloyId7;
-    var __alloyId14 = [];
-    var __alloyId15 = {
+    __alloyId9.push(__alloyId11);
+    var __alloyId13 = {
         type: "Ti.UI.Label",
         bindId: "dollarSign",
         properties: {
-            left: 60,
-            color: "black",
-            right: 300,
-            top: 6,
-            width: 48,
-            height: 48,
+            left: 220,
+            color: "#008000",
+            font: {
+                fontSize: 20
+            },
             bindId: "dollarSign"
         }
     };
-    __alloyId14.push(__alloyId15);
-    var __alloyId16 = {
+    __alloyId9.push(__alloyId13);
+    var __alloyId8 = {
+        properties: {
+            height: "80",
+            name: "checkedInTemplate",
+            accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_CHECKMARK
+        },
+        childTemplates: __alloyId9
+    };
+    __alloyId7["checkedInTemplate"] = __alloyId8;
+    var __alloyId16 = [];
+    var __alloyId17 = {
         type: "Ti.UI.Label",
         bindId: "name",
         properties: {
             left: 60,
             color: "black",
-            top: 5,
+            top: 10,
             font: {
-                fontWeight: "bold"
+                fontWeight: "bold",
+                fontFamily: "Helvetica Neue",
+                fontSize: 15
             },
             bindId: "name"
         }
     };
-    __alloyId14.push(__alloyId16);
-    var __alloyId17 = {
+    __alloyId16.push(__alloyId17);
+    var __alloyId18 = {
         type: "Ti.UI.Label",
         bindId: "cityState",
         properties: {
             left: 60,
             color: "black",
             top: 25,
+            bottom: 5,
+            font: {
+                fontFamily: "Helvetica Neue",
+                fontSize: 12
+            },
             bindId: "cityState"
         }
     };
-    __alloyId14.push(__alloyId17);
-    var __alloyId13 = {
+    __alloyId16.push(__alloyId18);
+    var __alloyId20 = {
+        type: "Ti.UI.Label",
+        bindId: "dollarSign",
+        properties: {
+            left: 220,
+            color: "#008000",
+            font: {
+                fontSize: 20
+            },
+            bindId: "dollarSign"
+        }
+    };
+    __alloyId16.push(__alloyId20);
+    var __alloyId15 = {
         properties: {
             name: "notCheckedInTemplate",
-            height: "60"
+            height: "80"
         },
-        childTemplates: __alloyId14
+        childTemplates: __alloyId16
     };
-    __alloyId6["notCheckedInTemplate"] = __alloyId13;
+    __alloyId7["notCheckedInTemplate"] = __alloyId15;
     $.__views.section = Ti.UI.createListSection({
         id: "section"
     });
-    var __alloyId22 = Alloy.Collections["donor"] || donor;
-    __alloyId22.on("fetch destroy change add remove reset", __alloyId23);
-    var __alloyId24 = [];
-    __alloyId24.push($.__views.section);
+    var __alloyId25 = Alloy.Collections["donor"] || donor;
+    __alloyId25.on("fetch destroy change add remove reset", __alloyId26);
+    var __alloyId27 = [];
+    __alloyId27.push($.__views.section);
     $.__views.list = Ti.UI.createListView({
-        sections: __alloyId24,
-        templates: __alloyId6,
+        sections: __alloyId27,
+        templates: __alloyId7,
+        searchView: $.__views.__alloyId6,
         id: "list",
-        defaultItemTemplate: "odd"
+        defaultItemTemplate: "checkedInTemplate"
     });
     $.__views.__alloyId5.add($.__views.list);
     $.__views.listtab = Ti.UI.createTab({
         icon: "KS_nav_views.png",
         window: $.__views.__alloyId5,
-        title: "ListView",
+        title: "AllList",
         id: "listtab"
     });
     $.__views.listtab && $.addTopLevelView($.__views.listtab);
     exports.destroy = function() {
-        __alloyId22.off("fetch destroy change add remove reset", __alloyId23);
+        __alloyId25.off("fetch destroy change add remove reset", __alloyId26);
     };
     _.extend($, $.__views);
     _.extend($, exports);
